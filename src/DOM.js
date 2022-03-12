ec1e1ba4a37876ca1fae0be0ee59f90f841b6a93
Denis <k1selevde@yandex.ru>
2022-03-12 12:19:09 +0500

    task dom

--- src/DOM.js
+++ src/DOM.js
@@ -5,6 +5,11 @@
   Считаем, что всегда передается тег, допускающий вставку текста в качестве своего содержимого (P, DIV, I и пр.).
 */
 export function appendToBody(tag, content, count) {
+    for (var i = 0; i < count; i++) {
+        var element = document.createElement(tag)
+        element.innerHTML = content;
+        document.body.appendChild(element)
+    }
 }
 
 /*
@@ -15,6 +20,31 @@ export function appendToBody(tag, content, count) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function generateTree(childrenCount, level) {
+
+    const createTreeNodes = (parentNode, count, level) => {
+        for (var i = 0; i < count; i++) {
+            var elem = document.createElement('div')
+            elem.classList.add(`item_${level}`);
+
+            parentNode.appendChild(elem)
+        }
+    }
+
+    // проверка по-хорошему, что childrenCount > 0, level > 0
+    var root = document.createElement('div')
+    root.classList.add('item_1');
+
+    var bodyNode =  document.body
+    bodyNode.appendChild(root)
+
+    for (var i = 1; i < level; i++) {
+        for (var j = 0; j < Math.pow(childrenCount, i - 1); j++) {
+            var parent = bodyNode.getElementsByClassName(`item_${i}`)[j]
+            createTreeNodes(parent, childrenCount, i + 1)
+        }
+    }
+
+    return bodyNode.firstChild;
 }
 
 /*
@@ -26,4 +56,34 @@ export function generateTree(childrenCount, level) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function replaceNodes() {
+    const tree = generateTree(2,3)
+
+    var bodyNode = document.body
+    document.body.appendChild(tree)
+
+    //hard
+    var level = 2
+    var tagName = 'section'
+
+    var replaceTags = bodyNode.getElementsByClassName(`item_${level}`);
+
+
+    const insertChildrenToNode = (node, childrens) => {
+        Array.from(childrens).forEach(child => {
+            var clone = child.cloneNode(true)
+            node.appendChild(clone)
+        });
+    }
+
+    for (var elem of replaceTags) {
+        var newTag = document.createElement(tagName);
+        newTag.classList.add(`item_${level}`);
+
+        var elemChildrens = elem.children;
+        insertChildrenToNode(newTag, elemChildrens)
+
+        elem.parentNode.replaceChild(newTag, elem);
+    }
+
+    return  bodyNode.firstChild;
 }
